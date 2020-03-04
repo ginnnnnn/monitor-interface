13370048e1ca3f8ecbd2445d8e4a84324d3fb6bb
ginnnnnn <aginlo@hotmail.com>
2020-03-04 15:15:18 +0800

    Updates

---
+++ precache-manifest.961a0770080ce453a9cb2299905ef6a6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a627e41822783892d6b1d32be30460",
+    "url": "/monitor-interface/index.html"
+  },
+  {
+    "revision": "815107fb05078e35650d",
+    "url": "/monitor-interface/static/css/main.199b4388.chunk.css"
+  },
+  {
+    "revision": "25df1d4f2272a2fbadef",
+    "url": "/monitor-interface/static/js/2.74ce793f.chunk.js"
+  },
+  {
+    "revision": "6d3c4316041b5905aa31b3ff080c57e2",
+    "url": "/monitor-interface/static/js/2.74ce793f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815107fb05078e35650d",
+    "url": "/monitor-interface/static/js/main.8df74634.chunk.js"
+  },
+  {
+    "revision": "cd66c27842b65f986d65",
+    "url": "/monitor-interface/static/js/runtime-main.b7545304.js"
+  },
+  {
+    "revision": "791abe0a65cb4e1215cb05aaf9373038",
+    "url": "/monitor-interface/static/media/cross.791abe0a.png"
+  },
+  {
+    "revision": "e4e175213011f62653990bde0ed16340",
+    "url": "/monitor-interface/static/media/green.e4e17521.png"
+  },
+  {
+    "revision": "3f5d2d9c67f6e2668a51291437b356d5",
+    "url": "/monitor-interface/static/media/grey.3f5d2d9c.png"
+  },
+  {
+    "revision": "78131d20af5d5d7fab0bc6b0ee607cee",
+    "url": "/monitor-interface/static/media/lightgreen.78131d20.png"
+  },
+  {
+    "revision": "63226b2bd6ed382cfd811072966dbff3",
+    "url": "/monitor-interface/static/media/red.63226b2b.png"
+  },
+  {
+    "revision": "18797685835c70be5dc742273e122928",
+    "url": "/monitor-interface/static/media/yellow.18797685.png"
+  }
+]);
